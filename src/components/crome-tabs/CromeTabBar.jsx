fb660534a8f9688ca603f2258c696056eda73a03
VISHWA <150323933+vishwa-git123@users.noreply.github.com>
2026-09-15 10:51:38 +0530

    crome tab auto update test

--- src/components/crome-tabs/CromeTabBar.jsx
+++ src/components/crome-tabs/CromeTabBar.jsx
@@ -1,5 +1,5 @@
 // ChromeTabBar.jsx
-import { FiPlus } from "react-icons/fi";
+import { FiPlus, FiMail } from "react-icons/fi";
 import ChromeTab from "./CromeTab";
 import "../../styles/crometab.css";
 
@@ -23,6 +23,7 @@ export default function ChromeTabBar({ tabs, activeTab, setActiveTab, addTab, cl
         onClick={() => addTab()}
        >
         <FiPlus size={16} />
+        <FiMail size={16}  />
       </div>
       </div>
     </div>
